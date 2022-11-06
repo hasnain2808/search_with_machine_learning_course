9853a0a7ad13e3e12561034e758c9a02422e4ab9
Mohammad Hasnain Mohsin Rajan <hasnain2808@gmail.com>
2022-11-06 15:06:13 +0000

    feat: filter cats greater than

--- week2/createContentTrainingData.py
+++ week2/createContentTrainingData.py
@@ -5,6 +5,7 @@ from tqdm import tqdm
 import os
 import xml.etree.ElementTree as ET
 from pathlib import Path
+import pandas as pd
 
 def transform_name(product_name):
     # IMPLEMENT
@@ -61,9 +62,21 @@ def _label_filename(filename):
 if __name__ == '__main__':
     files = glob.glob(f'{directory}/*.xml')
     print("Writing results to %s" % output_file)
+    label_count = {}
     with multiprocessing.Pool() as p:
         all_labels = tqdm(p.imap(_label_filename, files), total=len(files))
-        with open(output_file, 'w') as output:
-            for label_list in all_labels:
-                for (cat, name) in label_list:
-                    output.write(f'__label__{cat} {name}\n')
+        
+        dataframes = []
+
+        for label_list in all_labels:
+            dataframes.append(pd.DataFrame(label_list, columns = ['cat', 'name']))
+
+        df = pd.concat(dataframes, axis=0)
+        
+        df_count = df.groupby('cat').name.count()
+
+        cat_list = df_count[df_count > min_products].index.tolist()
+
+        df = df[df['cat'].isin(cat_list)]
+        
+        df.to_csv(output_file,  sep=' ', header=False, index=False)
